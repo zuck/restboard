fbb435d9153ec20773e8075a4aecd5316b23e6d0
Emanuele Bertoldi <emanuele.bertoldi@heply.it>
2021-03-01 21:43:21 +0100

    feat: add RbPageContent

---
+++ src/components/RbPageContent.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page padding :class="$q.screen.lt.md ? 'q-gutter-xs' : 'q-gutter-lg'">
+    <q-breadcrumbs v-if="breadcrumbs">
+      <q-breadcrumbs-el to="/" icon="home" />
+      <q-breadcrumbs-el
+        v-for="bc in breadcrumbs"
+        :key="bc.path"
+        :label="bc.label"
+        :to="bc.path || null"
+      />
+    </q-breadcrumbs>
+    <slot></slot>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'RbPageContent',
+  props: {
+    breadcrumbs: Array
+  }
+}
+</script>
