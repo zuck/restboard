97541c1e396afdd5d25a4874054908ddf2c8bc6d
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2021-03-14 00:33:34 +0100

    feat: add layout for fullpage forms

---
+++ src/layouts/FullPageForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-layout view="hHh lpr fFf">
+    <q-page-container>
+      <div class="page-content fixed-center">
+        <router-view />
+      </div>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'FullPageForm',
+
+  data () {
+    return {
+    }
+  }
+}
+</script>
