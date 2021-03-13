0f8078c599294b358fc68a85b33cf10ac9bfd732
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2021-03-14 00:33:05 +0100

    feat: add visual identity

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,9 @@
 <template>
   <q-page class="flex flex-center">
     <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
+      src="~assets/logo.svg"
+      height="128"
+      :alt="productName"
     >
   </q-page>
 </template>
