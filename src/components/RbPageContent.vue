1835af6bc22eefba9eb5ed1c39b62b358a55684d
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2021-03-15 23:11:13 +0100

    feat: add support for title

--- src/components/RbPageContent.vue
+++ src/components/RbPageContent.vue
@@ -1,15 +1,20 @@
 <template>
-  <q-page padding :class="$q.screen.lt.md ? 'q-gutter-xs' : 'q-gutter-lg'">
-    <q-breadcrumbs v-if="breadcrumbs">
-      <q-breadcrumbs-el to="/" icon="home" />
-      <q-breadcrumbs-el
-        v-for="bc in breadcrumbs"
-        :key="bc.path"
-        :label="bc.label"
-        :to="bc.path || null"
-      />
-    </q-breadcrumbs>
-    <div class="column">
+  <q-page>
+    <q-card>
+      <q-card-section>
+        <q-breadcrumbs v-if="breadcrumbs">
+          <q-breadcrumbs-el to="/" icon="home" />
+          <q-breadcrumbs-el
+            v-for="bc in breadcrumbs"
+            :key="bc.path"
+            :label="bc.label"
+            :to="bc.path || null"
+          />
+        </q-breadcrumbs>
+        <h1 class="q-my-none q-mt-md text-h4">{{ title }}</h1>
+      </q-card-section>
+    </q-card>
+    <div class="column q-gutter-md q-pa-md">
       <slot></slot>
     </div>
   </q-page>
@@ -19,7 +24,8 @@
 export default {
   name: 'RbPageContent',
   props: {
-    breadcrumbs: Array
+    breadcrumbs: Array,
+    title: String
   }
 }
 </script>
