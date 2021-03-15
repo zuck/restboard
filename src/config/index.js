ee25c258645e053bdf8ce103141eb3aa9b2d918a
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2021-03-15 22:02:57 +0100

    feat: use mocked login API by default

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-export const AUTH_API_URL = process.env.AUTH_API_URL || 'http://localhost:3000/auth'
+export const AUTH_API_URL = process.env.AUTH_API_URL || 'https://api.mocki.io/v1/12c8d278'
 export const AUTH_JWT_STORAGE_KEY = process.env.AUTH_JWT_STORAGE_KEY || 'rb-auth-jwt'
 export const DEFAULT_LOCALE = 'it'
 export const LOCALES = [{
