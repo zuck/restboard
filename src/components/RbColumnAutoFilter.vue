78da19373b394eba4ff40624eabb6573157971d8
Emanuele Bertoldi <emanuele.bertoldi@heply.it>
2021-05-12 09:50:31 +0200

    refactor: imrpove autofilter layout

--- src/components/RbColumnAutoFilter.vue
+++ src/components/RbColumnAutoFilter.vue
@@ -1,48 +1,25 @@
 <template>
   <q-menu>
-    <div class="row no-gutter">
-      <div class="col">
-        <q-btn
-          flat
-          class="full-width"
-          @click="resetFilters(true)"
-        >
-          {{ $t('Select all') }}
-        </q-btn>
-      </div>
-      <div class="col">
-        <q-btn
-          flat
-          class="full-width"
-          @click="resetFilters(false)"
-        >
-          {{ $t('Deselect all') }}
-        </q-btn>
-      </div>
-    </div>
-    <q-list
-      dense
-      style="min-width: 100px"
-    >
-      <q-separator />
-      <q-item
-        v-for="item in filterKeys"
-        :key="item"
-        v-close-popup
-        clickable
-      >
-        <q-item-section
-          side
-          top
-        >
-          <q-checkbox
-            :value="filters[item]"
-            @input="toggleFilter(item)"
-          />
+    <q-list dense style="min-width: 100px">
+      <q-item v-for="item in filterKeys" :key="item" v-close-popup clickable>
+        <q-item-section side top>
+          <q-checkbox :value="filters[item]" @input="toggleFilter(item)" />
         </q-item-section>
         <q-item-section>{{ item }}</q-item-section>
       </q-item>
     </q-list>
+    <q-separator />
+    <div class="row no-gutter">
+      <div class="col">
+        <q-btn flat dense class="full-width" @click="resetFilters(true)">
+          {{ $t("Select all") }}
+        </q-btn>
+        <q-separator />
+        <q-btn flat dense class="full-width" @click="resetFilters(false)">
+          {{ $t("Deselect all") }}
+        </q-btn>
+      </div>
+    </div>
   </q-menu>
 </template>
 
@@ -80,10 +57,9 @@ export default {
     },
 
     resetFilters (value) {
-      Object.keys(this.filters || {})
-        .forEach(key => {
-          this.filters[key] = value
-        })
+      Object.keys(this.filters || {}).forEach(key => {
+        this.filters[key] = value
+      })
       this.$emit('input', this.filters)
     },
 
