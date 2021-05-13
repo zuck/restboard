163a07182c6169526216983b861044db2cec1e92
Emanuele Bertoldi <emanuele.bertoldi@heply.it>
2021-05-13 21:03:39 +0200

    refactor: add "carousel" mode to the data grid

---
+++ src/components/RbDataCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-card class="fit">
+    <q-card-section>
+      <div class="row items-center no-wrap">
+        <div class="col">
+          <slot name="title" :title="title">
+            <div class="text-h6">{{ title }}</div>
+          </slot>
+        </div>
+        <div class="col-auto">
+          <slot name="actions" :actions="actions">
+            <rb-action-menu :actions="actions" />
+          </slot>
+        </div>
+      </div>
+    </q-card-section>
+    <q-card-section>
+      <slot :content="content">{{ content }}</slot>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+import RbActionMenu from '../components/RbActionMenu'
+
+export default {
+  name: 'RbDataCard',
+  components: {
+    RbActionMenu
+  },
+  props: {
+    actions: Array,
+    title: String,
+    content: String
+  }
+}
+</script>
--- src/components/RbResourceDataGrid.vue
+++ src/components/RbResourceDataGrid.vue
@@ -1,36 +1,61 @@
 <template>
-  <div class="column">
-    <div class="row q-col-gutter-md">
-      <div v-for="item in items" :key="item[key]" class="col-xs-12 col-sm-4 col-md-3">
-        <q-card class="full-width">
-          <q-card-section>
-            <div class="row items-center no-wrap">
-              <div class="col">
-                <div class="text-h6">{{ item[resource.displayAttr] }}</div>
-              </div>
-              <div class="col-auto">
-                <rb-action-menu :actions="actions" />
-              </div>
-            </div>
-          </q-card-section>
-        </q-card>
+  <q-carousel
+    class="bg-transparent"
+    v-model="currentSlide"
+    :animated="noWrap"
+    :infinite="noWrap"
+    :autoplay="noWrap"
+    :padding="noWrap"
+    :arrows="noWrap"
+    transition-prev="jump-right"
+    transition-next="jump-left"
+    control-color="primary"
+    height="auto"
+  >
+    <q-carousel-slide
+      v-for="(itemRow, index) in itemRows"
+      class="bg-transparent column no-wrap"
+      :key="index"
+      :name="`slide-${index}`"
+    >
+      <div class="row q-col-gutter-md">
+        <div
+          v-for="item in itemRow"
+          :key="item[key]"
+          class="col-xs-12 col-sm-6 col-md-4 col-lg-3"
+        >
+          <slot
+            name="card"
+            :item="item"
+            :resource="resource"
+            :actions="actions"
+          >
+            <rb-data-card
+              :title="item[resource.displayAttr]"
+              :actions="actions"
+            />
+          </slot>
+        </div>
       </div>
-    </div>
-  </div>
+    </q-carousel-slide>
+  </q-carousel>
 </template>
 
 <script>
-import RbActionMenu from '../components/RbActionMenu'
+import RbDataCard from 'components/RbDataCard'
 
 export default {
   name: 'RbResourceDataGrid',
   components: {
-    RbActionMenu
+    RbDataCard
   },
   props: {
     resource: {
       type: Object,
       required: true
+    },
+    noWrap: {
+      type: Boolean
     }
   },
   computed: {
@@ -39,18 +64,49 @@ export default {
     },
     key () {
       return this.resource.key || 'id'
+    },
+    itemsPerRow () {
+      if (this.noWrap) {
+        if (this.$q.screen.lt.sm) {
+          return 1
+        }
+        if (this.$q.screen.lt.md) {
+          return 2
+        }
+        if (this.$q.screen.lt.lg) {
+          return 3
+        }
+        return 4
+      }
+      return this.items.length
+    },
+    itemRows () {
+      let start = 0
+      const length = this.items.length
+      const size = this.itemsPerRow
+      const res = []
+      while (start < length) {
+        const end = start + size
+        res.push(this.items.slice(start, end))
+        start = end
+      }
+      return res
     }
   },
   data () {
     return {
       items: [],
-      loading: true
+      loading: true,
+      currentSlide: ''
     }
   },
   mounted () {
     this.reloadData()
   },
   methods: {
+    restartCarousel () {
+      this.currentSlide = 'slide-0'
+    },
     async reloadData () {
       this.loading = true
       try {
@@ -65,11 +121,15 @@ export default {
         })
       }
       this.loading = false
+      this.restartCarousel()
     }
   },
   watch: {
     resource: function () {
       this.reloadData()
+    },
+    noWrap: function () {
+      this.restartCarousel()
     }
   }
 }
