2805504fc9e5455bc7300a0df9e2656a481cd700
Emanuele Bertoldi <emanuele.bertoldi@heply.it>
2021-05-13 20:57:49 +0200

    feat: add transparent, flat, bordered, padding props

--- src/components/RbPageContent.vue
+++ src/components/RbPageContent.vue
@@ -1,20 +1,34 @@
 <template>
-  <q-page>
-    <q-card>
-      <q-card-section>
-        <q-breadcrumbs v-if="breadcrumbs">
-          <q-breadcrumbs-el to="/" icon="home" />
-          <q-breadcrumbs-el
-            v-for="bc in breadcrumbs"
-            :key="bc.path"
-            :label="bc.label"
-            :to="bc.path || null"
-          />
-        </q-breadcrumbs>
-        <h1 class="q-my-none q-mt-xs text-h4">{{ title }}</h1>
+  <q-page :padding="padding">
+    <q-card
+      square
+      :flat="flat"
+      :bordered="bordered"
+      :class="headerCss"
+    >
+      <q-card-section horizontal class="q-pt-xs">
+        <q-card-section class="col">
+          <slot name="breadcrumbs" :breadcrumbs="breadcrumbs">
+            <q-breadcrumbs v-if="breadcrumbs" class="q-mb-sm">
+              <q-breadcrumbs-el to="/" icon="home" />
+              <q-breadcrumbs-el
+                v-for="bc in breadcrumbs"
+                :key="bc.path"
+                :label="bc.label"
+                :to="bc.path || null"
+              />
+            </q-breadcrumbs>
+          </slot>
+          <slot name="title" :title="title">
+            <h1 class="q-mt-none q-mb-xs text-h1">{{ title }}</h1>
+          </slot>
+        </q-card-section>
+        <q-card-actions class="col-auto">
+          <slot name="actions"></slot>
+        </q-card-actions>
       </q-card-section>
     </q-card>
-    <div class="column q-gutter-md q-pa-md">
+    <div class="column q-px-md no-wrap">
       <slot></slot>
     </div>
   </q-page>
@@ -25,7 +39,18 @@ export default {
   name: 'RbPageContent',
   props: {
     breadcrumbs: Array,
-    title: String
+    title: String,
+    transparent: Boolean,
+    flat: Boolean,
+    bordered: Boolean,
+    padding: Boolean
+  },
+  computed: {
+    headerCss () {
+      return {
+        'bg-transparent': this.transparent
+      }
+    }
   }
 }
 </script>
